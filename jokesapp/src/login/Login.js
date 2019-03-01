30a1bd19f81ccc3e96c14c9ecb4a125d4e869e7e
Henry Neal <henry.neal0@gmail.com>
2019-03-01 13:04:19 -0500

    user can login but cannot see jokes yet

--- jokesapp/src/login/Login.js
+++ jokesapp/src/login/Login.js
@@ -50,7 +50,7 @@ class Login extends React.Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    const endpoint = 'http://localhost:3300/api/auth/login';
+    const endpoint = 'http://localhost:3300/api/login';
 
     axios
       .post(endpoint, this.state)
--- jokesapp/src/users/Users.js
+++ jokesapp/src/users/Users.js
@@ -22,9 +22,21 @@ class Users extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('/users').then(res => {
-      this.setState({ users: res.data.users });
-    });
+    function getJokes(req, res) {
+      const requestOptions = {
+        headers: { accept: 'application/json' },
+      };
+    
+      axios
+        .get('https://icanhazdadjoke.com/search', requestOptions)
+        .then(response => {
+          res.status(200).json(response.data.results);
+        })
+        .catch(err => {
+          res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+        });
+    }
+    
   }
 }
 
