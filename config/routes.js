7624453bbe00afd525d3ab58e949753151259fad
Henry Neal <henry.neal0@gmail.com>
2019-03-01 11:51:09 -0500

    login working

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,26 @@
 const axios = require('axios');
-
+require('dotenv').config();
+const Users = require('../users/user-model');
+const bcrypt = require('bcryptjs');
 const { authenticate } = require('../auth/authenticate');
+const db = require('../database/dbConfig');
+const jwt = require('jsonwebtoken');
+
+function generateToken(user) {
+  const payload = {
+    username: user.id,
+    name: user.username
+  };
+
+  const secret = process.env.JWT_SECRET;
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,11 +29,37 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => res.status(500).json(err));
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user); // new
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token...`,
+          token,
+          roles: token.roles,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
